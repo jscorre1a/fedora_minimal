a65f515c49c6b3f761d5dab7dd83e157461a6eed
jscorre1a <johnny@afterlife.lan>
2020-12-09 11:35:18 +0000

    Changes to be committed: 	modified:   install.sh Added sddm enable && aws-cli

--- install.sh
+++ install.sh
@@ -22,6 +22,24 @@ dnf install \
 	kernel-devel \
 	gcc
 
+systemctl enable sddm
 
+systemctl set-default graphical.target
+
+dnf config-manager --add-repo https://rpm.releases.hashicorp.com/$release/hashicorp.repo
+
+dnf install terraform
+
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+
+unzip awscliv2.zip
+
+./aws/install
+
+rm -rf aws
+
+rm -rf awscliv2.zip
+
+aws --version
 
 #udo rmmod kvm_intel kvm - probably not needed with --ignore-errors flag
