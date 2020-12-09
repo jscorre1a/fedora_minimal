111a9ac0cc975a145011df57bf9ad57b30e4c705
jscorre1a <johnny@afterlife.lan>
2020-12-09 11:17:25 +0000

    Changes to be committed: 	new file:   install.sh

---
+++ install.sh
@@ -0,0 +1,27 @@
+#!/bin/env bash
+
+dnf install \
+	bluedevil \
+	ark \
+	rofi \
+	tar \
+	dolphin \
+	firefox \
+	kvantum \
+	ansible \
+	sddm \
+	sddm-kcm \
+	kwin \
+	konsole \
+	plasma-desktop \
+	plasma-nm \
+	kde-settings-pulseaudio \
+	kscreen \
+	xorg-x11-drv-intel \
+	kernel-headers \
+	kernel-devel \
+	gcc
+
+
+
+#udo rmmod kvm_intel kvm - probably not needed with --ignore-errors flag
